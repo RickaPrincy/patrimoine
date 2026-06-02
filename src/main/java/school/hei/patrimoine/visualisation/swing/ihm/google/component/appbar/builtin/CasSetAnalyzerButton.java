8770c8669830aee5796a086e5b402863ccb91fc0
mihajatiana <hei.mihajatiana@gmail.com>
2026-06-02 23:39:35 +0300

    fix: ignore objectif global for recouped objectif non atteint

--- src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/component/appbar/builtin/CasSetAnalyzerButton.java
+++ src/main/java/school/hei/patrimoine/visualisation/swing/ihm/google/component/appbar/builtin/CasSetAnalyzerButton.java
@@ -1,5 +1,6 @@
 package school.hei.patrimoine.visualisation.swing.ihm.google.component.appbar.builtin;
 
+import static java.util.stream.Collectors.toSet;
 import static javax.swing.SwingUtilities.invokeLater;
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 import static school.hei.patrimoine.visualisation.swing.ihm.google.modele.MessageDialog.showError;
@@ -7,6 +8,7 @@ import static school.hei.patrimoine.visualisation.swing.ihm.google.modele.files.
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.swing.JMenuItem;
 import school.hei.patrimoine.cas.CasSet;
 import school.hei.patrimoine.cas.CasSetAnalyzer;
@@ -62,7 +64,7 @@ public class CasSetAnalyzerButton extends PopupMenuButton {
                         PatriLangFilesWatcher.getDoneCasSet())
                     .getRecouped())
         .onSuccess(result -> new CasSetAnalyzer(DISPOSE_ON_CLOSE).accept(result))
-        .onError(CasSetAnalyzerButton::handleError)
+        .onError(error -> handleError(error, List.of("Tout")))
         .build()
         .execute();
   }
@@ -78,10 +80,21 @@ public class CasSetAnalyzerButton extends PopupMenuButton {
   }
 
   private static void handleError(Exception error) {
+    handleError(error, List.of());
+  }
+
+  private static void handleError(Exception error, List<String> excludedNames) {
     error = error.getCause() == null ? error : (Exception) error.getCause();
     if (error instanceof ObjectifExeption exception) {
       var objectifs = exception.getObjectifNonAtteints();
-      invokeLater(() -> new ObjectifNonAtteintsDialog(objectifs));
+
+      var objectifsFiltrés =
+          objectifs.stream()
+              .filter(objectif -> !excludedNames.contains(objectif.objectivable().nom()))
+              .collect(toSet());
+      if (!objectifsFiltrés.isEmpty()) {
+        invokeLater(() -> new ObjectifNonAtteintsDialog(objectifsFiltrés));
+      }
       return;
     }
     showError(error);
